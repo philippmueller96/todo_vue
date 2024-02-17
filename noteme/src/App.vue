6f33e04d6e8d01835608ed22f4a9e27da0593341
Philipp Müller <philipp.mueller@hb-capital.de>
2024-02-17 12:51:00 +0100

    Added Props

--- noteme/src/App.vue
+++ noteme/src/App.vue
@@ -1,9 +1,44 @@
 <script setup lang="ts">
 import Header from "./components/Header.vue";
+import AllNotes from "./components/AllNotes.vue";
+
+const notesList = [
+  {
+    id: "1",
+    title: "Katzenfutter kaufen",
+    description:
+      "Katzenfutter von Rossmann kaufen. Sorte: Winston Gourmet Hähnchen-Karotte",
+    creationDate: "16.02.2024",
+    deadline: "01.03.2024",
+    location: "Leipzig",
+    finished: false,
+  },
+  {
+    id: "2",
+    title: "Staubsaugen",
+    description: "",
+    creationDate: "16.02.2024",
+    deadline: "18.02.2024",
+    location: "Zuhause",
+    finished: false,
+  },
+  {
+    id: "3",
+    title: "Lecker Bierchen",
+    description:
+      "Janosch und Sven anschreiben und nach Bierchen und Billard oder so fragen",
+    creationDate: "16.02.2024",
+    deadline: "16.02.2024",
+    location: "",
+    finished: false,
+  },
+];
 </script>
 
 <template>
+  <div></div>
   <Header />
+  <AllNotes :notes="notesList" />
 </template>
 
 <style scoped></style>
